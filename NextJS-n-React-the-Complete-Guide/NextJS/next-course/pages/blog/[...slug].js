b2a9570f0826ba46364dd5ebf1b9ff40fdb9765f
saviorocha <42saviorocha@gmail.com>
2021-12-02 10:48:50 -0300

    Section 3: Pages & File-based Routing

---
+++ NextJS-n-React-the-Complete-Guide/NextJS/next-course/pages/blog/[...slug].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const BlogPostsPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>BlogPostsPage</h1>
+    </div>
+  );
+};
+
+export default BlogPostsPage;
